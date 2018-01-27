49f9958b6534d881c310c13a6df44ee0499ecd4f
Hendrik Demmer <hendrik.demmer@gmail.com>
2018-01-27 12:12:19 +0100

    ship controls

--- js/game.js
+++ js/game.js
@@ -3,7 +3,10 @@
 var ctx;
 var sprites = {};
 
-var boat = [0,0];
+var boatAngle = 0;
+var boatSpeed = 0;
+var boatX = 0;
+var boatY = 0;
 
 function start() {
     var canvas = document.getElementById("thecanvas");
@@ -18,16 +21,29 @@ function start() {
     drawScene();
 }
 
+function drawSprite(name,x,y,angle)
+{
+	var image = sprites[name];
+	ctx.save();
+	ctx.translate(x, y);
+	ctx.rotate(angle);
+	ctx.translate(-0.5 * image.width,-0.5 * image.height);
+	ctx.drawImage(image,0,0);
+	ctx.restore();
+}
+
 function drawScene()
 {
+	if (getKey("left")) boatAngle -= 0.01;
+	if (getKey("right")) boatAngle += 0.01;
+	if (getKey("up")) boatSpeed += 0.1;
+	if (getKey("down")) boatSpeed -= 0.1;
 
-	if (getKey("left")) boat[0] -=1;
-	if (getKey("right")) boat[0] +=1;
-	if (getKey("up")) boat[1] -=1;
-	if (getKey("down")) boat[1] +=1;
-
+	boatX += Math.cos(boatAngle) * boatSpeed;
+	boatY += Math.sin(boatAngle) * boatSpeed;
 
 	ctx.drawImage(sprites["map"],10,10); 
-	ctx.drawImage(sprites["boat"],boat[0],boat[1]); 
+	drawSprite("boat",boatX,boatY, boatAngle);
+
 	window.requestAnimationFrame(drawScene);
 }
